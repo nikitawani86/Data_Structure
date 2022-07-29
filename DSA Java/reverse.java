08aecc0cecc5c83228d1b4b3872d84c9ecb78889
nikitawani86 <57154930+nikitawani86@users.noreply.github.com>
2022-07-30 00:42:21 +0530

    Add files via upload

---
+++ DSA Java/reverse.java
@@ -0,0 +1,16 @@
+
+public class reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Reverse the array 
+		int a[] = {3,6,2,1,8};
+		for(int i=a.length-1;i>=0;i--) {
+			System.out.println(a[i]);
+		}
+		
+		 
+		
+	}
+
+}
